d913b96833514bf55cdd388f10c2d0a46537a796
brouki171 <ian718595@gmail.com>
2026-09-16 00:41:49 -0700

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -1,39 +1,13 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCDirector.hpp>
-
-#import <QuartzCore/QuartzCore.h>
-#import <UIKit/UIKit.h>
 
 using namespace geode::prelude;
 
-// Force Geometry Dash's director to use a 120 FPS animation interval.
-class $modify(iOS120HzDirector, cocos2d::CCDirector) {
-    void setAnimationInterval(double interval) {
-        CCDirector::setAnimationInterval(1.0 / 120.0);
-        log::debug("[iOS120Hz] Animation interval forced to 120Hz");
-    }
-};
-
-// Configure the device's display for 120 Hz.
 $on_mod(Loaded) {
-    log::info("[iOS120Hz] Mod loaded - 120Hz support enabled");
+    log::info("[iOS120Hz TEST] DLL LOADED SUCCESSFULLY");
 
-    UIScreen* screen = [UIScreen mainScreen];
-
-    if (@available(iOS 15.0, *)) {
-        NSInteger maxFPS = screen.maximumFramesPerSecond;
-
-        log::info(
-            "[iOS120Hz] Device maximum frame rate: {}Hz",
-            maxFPS
-        );
-
-        if (maxFPS >= 120) {
-            log::info("[iOS120Hz] 120Hz display detected");
-        } else {
-            log::warn(
-                "[iOS120Hz] Device does not support a 120Hz display"
-            );
-        }
-    }
+    FLAlertLayer::create(
+        "iOS 120Hz Test",
+        "The iOS 120Hz mod DLL is running!",
+        "OK"
+    )->show();
 }
