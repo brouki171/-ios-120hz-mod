9f6b6f2ad9ee9d27d9ba43e161947088b36d49f2
brouki171 <ian718595@gmail.com>
2026-09-15 23:38:57 -0700

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -1,5 +1,4 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/AppDelegate.hpp>
 #include <Geode/modify/CCDirector.hpp>
 
 #import <QuartzCore/QuartzCore.h>
@@ -7,99 +6,31 @@
 
 using namespace geode::prelude;
 
-// Force Geometry Dash's main display link to prefer 120 Hz.
-static void configure120HzDisplayLink(CADisplayLink* displayLink) {
-    if (!displayLink) {
-        return;
-    }
-
-    if (@available(iOS 15.0, *)) {
-        displayLink.preferredFrameRateRange =
-            CAFrameRateRangeMake(120.0, 120.0, 120.0);
-    } else {
-        displayLink.preferredFramesPerSecond = 120;
-    }
-}
-
-// Hook into AppDelegate.
-class $modify(iOS120HzAppDelegate, AppDelegate) {
-    bool applicationDidFinishLaunching() {
-        if (!AppDelegate::applicationDidFinishLaunching()) {
-            return false;
-        }
-
-        log::info("[iOS120Hz] Initializing 120Hz support...");
-
-        UIApplication* app = [UIApplication sharedApplication];
-        UIWindow* keyWindow = nil;
-
-        for (UIWindow* window in app.windows) {
-            if (window.isKeyWindow) {
-                keyWindow = window;
-                break;
-            }
-        }
-
-        if (!keyWindow && app.windows.count > 0) {
-            keyWindow = app.windows[0];
-        }
-
-        if (keyWindow) {
-            UIScreen* screen = keyWindow.screen;
-
-            if (@available(iOS 15.0, *)) {
-                log::info(
-                    "[iOS120Hz] Device maximum frame rate: {}Hz",
-                    screen.maximumFramesPerSecond
-                );
-            }
-
-            // Create a display link that runs on the main run loop.
-            CADisplayLink* displayLink =
-                [CADisplayLink displayLinkWithTarget:keyWindow
-                                             selector:@selector(iOS120Hz_tick:)];
-
-            if (displayLink) {
-                configure120HzDisplayLink(displayLink);
-
-                [displayLink addToRunLoop:[NSRunLoop mainRunLoop]
-                                  forMode:NSDefaultRunLoopMode];
-
-                log::info("[iOS120Hz] Display link configured for 120Hz");
-            }
-        }
-
-        return true;
-    }
-};
-
-// Hook CCDirector to force the game animation interval to 120 FPS.
-class $modify(iOS120HzDirector, CCDirector) {
+// Force Geometry Dash's director to use a 120 FPS animation interval.
+class $modify(iOS120HzDirector, cocos2d::CCDirector) {
     void setAnimationInterval(double interval) {
         CCDirector::setAnimationInterval(1.0 / 120.0);
         log::debug("[iOS120Hz] Animation interval forced to 120Hz");
     }
-
-    void setNextDeltaTimeZero(bool nextDeltaTimeZero) {
-        CCDirector::setNextDeltaTimeZero(nextDeltaTimeZero);
-    }
 };
 
-// Mod initialization.
+// Configure the device's display for 120 Hz.
 $on_mod(Loaded) {
     log::info("[iOS120Hz] Mod loaded - 120Hz support enabled");
 
-    UIScreen* mainScreen = [UIScreen mainScreen];
+    UIScreen* screen = [UIScreen mainScreen];
 
     if (@available(iOS 15.0, *)) {
-        NSInteger maxFPS = mainScreen.maximumFramesPerSecond;
+        NSInteger maxFPS = screen.maximumFramesPerSecond;
 
         log::info(
             "[iOS120Hz] Device maximum frame rate: {}Hz",
             maxFPS
         );
 
-        if (maxFPS < 120) {
+        if (maxFPS >= 120) {
+            log::info("[iOS120Hz] 120Hz display detected");
+        } else {
             log::warn(
                 "[iOS120Hz] Device does not support a 120Hz display"
             );
